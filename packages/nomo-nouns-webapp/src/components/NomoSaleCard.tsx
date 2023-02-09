4631dde8aaf7eeb50b35a230bf7cb05af0dfac34
Volky <volkyeth@proton.me>
2023-02-09 08:30:59 -0300

    Add fomo + NOC app iframes

---
+++ packages/nomo-nouns-webapp/src/components/NomoSaleCard.tsx
@@ -0,0 +1,186 @@
+import { NomoCard } from "../NomoCard";
+import {
+  Button,
+  HStack,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Spinner,
+  StackProps,
+  Stat,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  VStack,
+} from "@chakra-ui/react";
+import { formatEther } from "ethers/lib/utils";
+import { FC, useState } from "react";
+import { FinishedMatch, getMintPrice, SellingMatch } from "../../common/match";
+import { BigNumber } from "ethers";
+import { useQuery } from "react-query";
+import { useTimestamp } from "../hooks/useTimestamp";
+import { useAccount, useBalance } from "wagmi";
+import { formatCountdown } from "../utils/formatCountdown";
+import { useMintNomo, useStartNextMatch } from "../hooks/match";
+
+export type NomoSaleCardProps = {
+  match: SellingMatch | FinishedMatch;
+} & StackProps;
+
+export const NomoSaleCard: FC<NomoSaleCardProps> = ({ match, ...props }) => {
+  const [isMinting, setIsMinting] = useState(false);
+  const { data: ethPrice, isSuccess: ethPriceAvailable } = useQuery(
+    "ethPrice",
+    () =>
+      fetch(
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+      )
+        .then((r) => r.json())
+        .then((r) => r.ethereum.usd),
+    { staleTime: 15 * 60_000 }
+  );
+  const now = useTimestamp();
+  const { isDisconnected, address } = useAccount();
+  const timeElapsedFromStart = now - match.startTime;
+  const secondsToNextPriceIncrease =
+    now < match.startTime
+      ? match.startTime - now + match.mintingIncreaseInterval
+      : match.mintingIncreaseInterval -
+        (timeElapsedFromStart % match.mintingIncreaseInterval);
+  const priceIncreaseCountdown = formatCountdown(secondsToNextPriceIncrease);
+
+  const { data: balance } = useBalance({ addressOrName: address });
+  const [mintQuantity, setMintQuantity] = useState(1);
+  const { canMint, mintNomo } = useMintNomo(match);
+  const onMint =
+    mintNomo === undefined
+      ? undefined
+      : () => {
+          setIsMinting(true);
+          mintNomo(mintQuantity).finally(() => setIsMinting(false));
+        };
+  const mintPrice = getMintPrice(now, match);
+  const totalCost = mintPrice.mul(mintQuantity);
+  const totalCostString = formatEther(totalCost);
+  const hasFundsToMint = !balance || balance.value.gte(totalCost);
+  const saleOver = match.status === "Finished";
+  return (
+    <VStack alignItems={"center"} spacing={0} {...props}>
+      <NomoCard
+        w={"full"}
+        nounId={match.nounId}
+        tally={match.electedNomoTally}
+        match={match}
+        mb={0}
+        borderWidth={1}
+        borderBottomWidth={0}
+        borderBottomRadius={0}
+      />
+      <VStack w={"full"} p={0}>
+        <VStack
+          w={"full"}
+          borderWidth={1}
+          borderTopWidth={0}
+          p={0}
+          borderRadius={"md"}
+          borderTopRadius={0}
+          spacing={0}
+        >
+          <HStack p={4} w={"full"} justifyContent={"space-between"}>
+            <Stat textAlign={saleOver ? "center" : undefined} w={10}>
+              <StatLabel>{saleOver ? "Last price" : "Current Price"}</StatLabel>
+              <StatNumber>{formatEther(mintPrice)} ETH</StatNumber>
+              <StatHelpText mb={0}>
+                {ethPriceAvailable ? (
+                  `${getUsdPrice(mintPrice, ethPrice)} USD`
+                ) : (
+                  <Spinner />
+                )}
+              </StatHelpText>
+            </Stat>
+            {!saleOver && (
+              <Stat textAlign={"end"}>
+                <StatLabel>Price in {priceIncreaseCountdown}</StatLabel>
+                <StatNumber>
+                  {formatEther(
+                    mintPrice.add(match.mintingPriceIncreasePerInterval)
+                  )}{" "}
+                  ETH
+                </StatNumber>
+                <StatHelpText mb={0}>
+                  {ethPriceAvailable ? (
+                    `${getUsdPrice(
+                      mintPrice.add(match.mintingPriceIncreasePerInterval),
+                      ethPrice
+                    )} USD`
+                  ) : (
+                    <Spinner />
+                  )}
+                </StatHelpText>
+              </Stat>
+            )}
+          </HStack>
+          <HStack w={"full"} spacing={0}>
+            <Button
+              disabled={
+                isDisconnected ||
+                !hasFundsToMint ||
+                !canMint ||
+                isMinting ||
+                saleOver
+              }
+              isLoading={isMinting}
+              loadingText={`Minting ${mintQuantity}`}
+              onClick={onMint}
+              borderRadius={0}
+              w={"full"}
+            >
+              {saleOver
+                ? "Sale over"
+                : isDisconnected
+                ? "Connect to mint"
+                : !hasFundsToMint
+                ? `Wallet does not have ${totalCostString}ETH`
+                : `Mint ${mintQuantity} now for ${totalCostString}ETH`}
+            </Button>
+            {!saleOver && (
+              <NumberInput
+                // @ts-ignore
+                disabled={isMinting}
+                value={mintQuantity}
+                onChange={(_, valueAsNumber) => {
+                  if (!valueAsNumber || !(valueAsNumber > 0)) {
+                    setMintQuantity(1);
+                  } else {
+                    setMintQuantity(valueAsNumber);
+                  }
+                }}
+                min={1}
+              >
+                <NumberInputField
+                  w={20}
+                  borderLeftRadius={0}
+                  borderTopRadius={0}
+                  borderRightWidth={0}
+                  borderBottomWidth={0}
+                />
+                <NumberInputStepper borderTopRadius={0}>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            )}
+          </HStack>
+        </VStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+const getUsdPrice = (mintPrice: BigNumber, ethUsdPrice: number) =>
+  (
+    (mintPrice.div(1_000_000_000).toNumber() * ethUsdPrice) /
+    1_000_000_000
+  ).toFixed(2);
--- packages/nomo-nouns-webapp/src/components/SellingPhase.tsx
+++ packages/nomo-nouns-webapp/src/components/SellingPhase.tsx
@@ -1,9 +1,10 @@
 import { FinishedMatch, getMintPrice, SellingMatch } from "../../common/match";
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
 import { useTimestamp } from "../hooks/useTimestamp";
 import { useAccount, useBalance } from "wagmi";
 import { formatEther } from "ethers/lib/utils";
+import fomoCssOverrides from "./fomo-overrides.css";
 import {
   Button,
   HStack,
@@ -20,192 +21,81 @@ import {
   StatNumber,
   Text,
   VStack,
+  chakra,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import { NomoCard } from "../NomoCard";
 import { PreviousNomos } from "./PreviousNomos";
 import { formatCountdown } from "../utils/formatCountdown";
 import { useMintNomo, useStartNextMatch } from "../hooks/match";
 import { BigNumber } from "ethers";
+import { NomoSaleCard } from "./NomoSaleCard";
 
 export type SellingPhaseProps = {
   match: SellingMatch | FinishedMatch;
 };
 
 export const SellingPhase: FC<SellingPhaseProps> = ({ match }) => {
-  const [isMinting, setIsMinting] = useState(false);
-  const { data: ethPrice, isSuccess: ethPriceAvailable } = useQuery(
-    "ethPrice",
-    () =>
-      fetch(
-        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
-      )
-        .then((r) => r.json())
-        .then((r) => r.ethereum.usd),
-    { staleTime: 15 * 60_000 }
-  );
-  const now = useTimestamp();
-  const { isDisconnected, address } = useAccount();
+  const { isDisconnected } = useAccount();
   const saleOver = match.status === "Finished";
-  const timeElapsedFromStart = now - match.startTime;
-  const secondsToNextPriceIncrease =
-    now < match.startTime
-      ? match.startTime - now + match.mintingIncreaseInterval
-      : match.mintingIncreaseInterval -
-        (timeElapsedFromStart % match.mintingIncreaseInterval);
-  const priceIncreaseCountdown = formatCountdown(secondsToNextPriceIncrease);
-
-  const { data: balance } = useBalance({ addressOrName: address });
-  const [mintQuantity, setMintQuantity] = useState(1);
   const nextMatch = useStartNextMatch();
-  const { canMint, mintNomo } = useMintNomo(match);
-  const mintPrice = getMintPrice(now, match);
-  const totalCost = mintPrice.mul(mintQuantity);
-  const totalCostString = formatEther(totalCost);
-  const hasFundsToMint = !balance || balance.value.gte(totalCost);
-  const onMint =
-    mintNomo === undefined
-      ? undefined
-      : () => {
-          setIsMinting(true);
-          mintNomo(mintQuantity).finally(() => setIsMinting(false));
-        };
-
   // @ts-ignore
   return (
     <VStack h={"full"} spacing={[6, 10]} w={"full"}>
-      <VStack spacing={0}>
-        <Text fontSize={["md", "xl"]}>
-          {saleOver
-            ? `The sale for Nomo Noun #${match.nounId} is over`
-            : `Mint Nomo Noun #${match.nounId}!`}
-        </Text>
-        <Text fontSize={["xs", "md"]}>
-          {saleOver
-            ? `The match for Nomo Noun #${match.nounId + 1} starts soon 👀`
-            : `The sooner you mint, the cheaper it is!`}
-        </Text>
-      </VStack>
-
-      <VStack
-        alignItems={["center", "start"]}
-        spacing={0}
-        lineHeight={"normal"}
+      <SimpleGrid
+        w={"full"}
+        templateAreas={[
+          saleOver
+            ? `"noun-title"
+           "noun"
+           "nomo-title"
+           "nomo"`
+            : `"nomo-title"
+           "nomo"
+           "noun-title"
+           "noun"`,
+          `"noun-title nomo-title"
+           "noun nomo"`,
+        ]}
+        templateColumns={["352px", "352px 352px"]}
+        gridGap={4}
+        justifyContent={"center"}
+        justifyItems={"center"}
+        alignItems={"end"}
       >
-        <Text fontSize={"lg"} fontWeight={"bold"}>
-          Nomo Noun #{match.nounId}
+        <Text gridArea={"noun-title"} fontSize={"lg"} fontWeight={"bold"}>
+          {saleOver ? "Pick the next Noun!" : "Watch the Nouns auction"}
         </Text>
-        <Text fontSize={"sm"}>
-          Born {new Date(match.matchEnd * 1000).toDateString()}
-        </Text>
-        <Stack
-          direction={["column", "row"]}
-          alignItems={["center", "start"]}
-          spacing={[2, 6]}
+        <VStack
+          gridArea={"nomo-title"}
+          alignItems={"center"}
+          spacing={0}
+          lineHeight={"normal"}
         >
-          <NomoCard
-            w={["192px", "320px"]}
-            nounId={match.nounId}
-            tally={match.electedNomoTally}
-            match={match}
-          />
-          <VStack>
-            <VStack
-              w={["xs", "sm"]}
-              borderWidth={1}
-              borderRadius={"md"}
-              spacing={0}
-            >
-              <HStack p={4} w={"full"} justifyContent={"space-between"}>
-                <Stat textAlign={saleOver ? "center" : undefined}>
-                  <StatLabel>
-                    {saleOver ? "Last price" : "Current Price"}
-                  </StatLabel>
-                  <StatNumber>{formatEther(mintPrice)} ETH</StatNumber>
-                  <StatHelpText mb={0}>
-                    {ethPriceAvailable ? (
-                      `${getUsdPrice(mintPrice, ethPrice)} USD`
-                    ) : (
-                      <Spinner />
-                    )}
-                  </StatHelpText>
-                </Stat>
-                {!saleOver && (
-                  <Stat textAlign={"end"}>
-                    <StatLabel>Price in {priceIncreaseCountdown}</StatLabel>
-                    <StatNumber>
-                      {formatEther(
-                        mintPrice.add(match.mintingPriceIncreasePerInterval)
-                      )}{" "}
-                      ETH
-                    </StatNumber>
-                    <StatHelpText mb={0}>
-                      {ethPriceAvailable ? (
-                        `${getUsdPrice(
-                          mintPrice.add(match.mintingPriceIncreasePerInterval),
-                          ethPrice
-                        )} USD`
-                      ) : (
-                        <Spinner />
-                      )}
-                    </StatHelpText>
-                  </Stat>
-                )}
-              </HStack>
-              <HStack w={"full"} spacing={0}>
-                <Button
-                  disabled={
-                    isDisconnected ||
-                    !hasFundsToMint ||
-                    !canMint ||
-                    isMinting ||
-                    saleOver
-                  }
-                  isLoading={isMinting}
-                  loadingText={`Minting ${mintQuantity}`}
-                  onClick={onMint}
-                  borderRadius={0}
-                  w={"full"}
-                >
-                  {saleOver
-                    ? "Sale over"
-                    : isDisconnected
-                    ? "Connect to mint"
-                    : !hasFundsToMint
-                    ? `Wallet does not have ${totalCostString}ETH`
-                    : `Mint ${mintQuantity} now for ${totalCostString}ETH`}
-                </Button>
-                {!saleOver && (
-                  <NumberInput
-                    // @ts-ignore
-                    disabled={isMinting}
-                    value={mintQuantity}
-                    onChange={(_, valueAsNumber) => {
-                      if (!valueAsNumber || !(valueAsNumber > 0)) {
-                        setMintQuantity(1);
-                      } else {
-                        setMintQuantity(valueAsNumber);
-                      }
-                    }}
-                    min={1}
-                  >
-                    <NumberInputField
-                      w={20}
-                      borderLeftRadius={0}
-                      borderTopRadius={0}
-                      borderRightWidth={0}
-                      borderBottomWidth={0}
-                    />
-                    <NumberInputStepper borderTopRadius={0}>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                )}
-              </HStack>
-            </VStack>
-          </VStack>
-        </Stack>
-      </VStack>
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Nomo Noun #{match.nounId}
+          </Text>
+          <Text fontSize={"sm"}>
+            Born {new Date(match.matchEnd * 1000).toDateString()}
+          </Text>
+          {!saleOver && (
+            <Text fontSize={["xs", "sm"]}>
+              The sooner you mint, the cheaper it is!
+            </Text>
+          )}
+        </VStack>
+        <chakra.iframe
+          gridArea={"noun"}
+          id={"noun"}
+          borderRadius={"md"}
+          w={"full"}
+          h={"520px"}
+          src={
+            saleOver ? "https://fomonouns.wtf/" : "https://www.nounoclock.app/"
+          }
+        />
+        <NomoSaleCard gridArea={"nomo"} h={"520px"} w={"full"} match={match} />
+      </SimpleGrid>
 
       {!import.meta.env.PROD && (
         <Button disabled={isDisconnected} onClick={nextMatch}>
@@ -217,9 +107,3 @@ export const SellingPhase: FC<SellingPhaseProps> = ({ match }) => {
     </VStack>
   );
 };
-
-const getUsdPrice = (mintPrice: BigNumber, ethUsdPrice: number) =>
-  (
-    (mintPrice.div(1_000_000_000).toNumber() * ethUsdPrice) /
-    1_000_000_000
-  ).toFixed(2);
